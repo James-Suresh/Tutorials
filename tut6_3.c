ede173653ed66d6c15f686b1c356780f5bfc93de
James-Suresh <jamesblesson@gmail.com>
2019-03-23 23:42:46 -0800

    commiting

--- tut6_3.c
+++ tut6_3.c
@@ -9,39 +9,55 @@
 
 int moving_sum[5];
 
+
 struct numinfo{
 int number;
 int index;
+};
+void *factorial(void *n){
+  printf("hi%d\n",((struct numinfo*)n)->number );
+pthread_exit(NULL);
 }
 
-void* factorial(void *arg){
-  printf("hi\n" );
-}
 int main()
 {
 pthread_t t1,t2,t3,t4,t5;
-
+int in;
 
 
 printf("Enter the first number\n");
 
-getchar();
-  struct args numinfo *n = (struct numinfo *)malloc(sizeof(struct numinfo));
-  n->number= getchar()- '0';
-  n->index=1;
-pthread_create(&t1, NULL, &factorial, (void *)n);
-//pthread_join(t1, NULL);
+
+struct  numinfo *n = (struct numinfo *)malloc(sizeof(struct numinfo));
+scanf("%d", &n->number);
+n->index=1;
+pthread_create(&t1, NULL, factorial, (void *)n);
+pthread_join(t1, NULL);
+
 
 printf("Enter the second number\n");
-moving_sum[1]= getchar()- '0';
-getchar();
+scanf("%d", &n->number);
+n->index=1;
+pthread_create(&t2, NULL, factorial, (void *)n);
+pthread_join(t2, NULL);
+
+
+
 printf("Enter the third number\n");
-moving_sum[2]= getchar()- '0';
-getchar();
+scanf("%d", &n->number);
+n->index=1;
+pthread_create(&t3, NULL, factorial, (void *)n);
+pthread_join(t3, NULL);
+
 printf("Enter the fourth number\n");
-moving_sum[3] = getchar()- '0';
-getchar();
+scanf("%d", &n->number);
+n->index=1;
+pthread_create(&t4, NULL, factorial, (void *)n);
+pthread_join(t4, NULL);
+
 printf("Enter the fifth number\n");
-moving_sum[4]= getchar()- '0';
-getchar();
+scanf("%d", &n->number);
+n->index=1;
+pthread_create(&t5, NULL, factorial, (void *)n);
+pthread_join(t5, NULL);
 }
